589cd449ec1ae8f6ad5f15bc490257fc889b3655
vitorlindoperfeito <137408808+vitorlindoperfeito@users.noreply.github.com>
2023-08-31 09:19:31 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+tempo = 5000;
+const idade = 18;
+age = 18;
+age = age + 1;
+var time =setInterval(chamar,tempo);
+
+function chamar(){
+//documento.getElement so e chamado com tempo
+    document.getElementById("mostrar").innerHTML = idade;
+    document.getElementById("exibir").innerHTML = age;
+
+}
---
+++ regredir.js
@@ -0,0 +1,20 @@
+valor = 10;
+contagem();
+function contagem(){
+
+if (valor > 0){
+while (valor >= 0){
+   document.write(valor + "<br>")
+ valor--;                              
+}}}
+
+valor2 = 10;
+
+contar();
+function contar(){
+
+if(valor2 <=10 ){
+while(valor2 <= 20){
+    document.write(valor2 + "<br>");
+    valor2++;
+}}}
